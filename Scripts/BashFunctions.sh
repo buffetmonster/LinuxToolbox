619322439e4ca48423ef3baa41ee4116ab5bf2ff
davey <davey.shop.stuff@gmail.com>
2023-07-19 11:09:06 +0100

    added go_network_checkx

--- Scripts/BashFunctions.sh
+++ Scripts/BashFunctions.sh
@@ -1,4 +1,11 @@
-#ALL NEW SCRIPTS 2023
+#ALL NEW SCRIPTS 2023!
+
+# Various colours
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+GREEN="\033[0;32m"
+CYAN="\033[0;36m"
+NC="\033[0m"
 
 LS_COLORS='rs=0:di=1;35:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lz=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.axa=00;36:*.oga=00;36:*.spx=00;36:*.xspf=00;36:';
 export LS_COLORS
@@ -233,7 +240,7 @@ else #NOT RUNNING ON SLAB
     echo $runcmd
     powershell.exe $runcmd
     }
-fi
+fi #NOT RUNNING ON SLAB
 #common bash functions can go here
 go_quotax()
 {
@@ -282,6 +289,23 @@ go_set_title_bannerx(){
 #echo -e '\033]2;'$mytitle'\007'
 echo -e '\033]2;'$*'\007'
 }
+go_network_checkx(){
+if (ifconfig | grep "eth01" > /dev/null); then
+MyNet="Network eth0"
+elif (ifconfig | grep "inet" > /dev/null); then
+MyNet="Network (but no eth0)"
+else
+echo -e "${RED}WARNING${NC}:Network down\n"
+return 1
+fi
+if wget -q --spider http://google.com ; then
+echo "$MyNet up & Online"
+else
+echo -e "$MyNet up & ${RED}Offline${NC}\n"
+fi
+}
+#Scripts to run by default
+go_network_checkx
 return 0
 
 
